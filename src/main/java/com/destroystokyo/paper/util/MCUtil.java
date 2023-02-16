0963b4cb7ae26cef0c1767ca6985cbffd5fbfae7
manugame <manugameur.pro@gmail.com>
2023-02-17 00:23:51 +0100

    Fix async chunk save CME

---
+++ src/main/java/com/destroystokyo/paper/util/MCUtil.java
@@ -0,0 +1,36 @@
+package com.destroystokyo.paper.util;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.ChunkPos;
+
+import java.util.List;
+
+public class MCUtil {
+
+    public static List<ChunkPos> getSpiralOutChunks(BlockPos blockposition, int radius) {
+        List<ChunkPos> list = com.google.common.collect.Lists.newArrayList();
+
+        list.add(new ChunkPos(blockposition.getX() >> 4, blockposition.getZ() >> 4));
+        for (int r = 1; r <= radius; r++) {
+            int x = -r;
+            int z = r;
+
+            // Iterates the edge of half of the box; then negates for other half.
+            while (x <= r && z > -r) {
+                list.add(new ChunkPos((blockposition.getX() + (x << 4)) >> 4, (blockposition.getZ() + (z << 4)) >> 4));
+                list.add(new ChunkPos((blockposition.getX() - (x << 4)) >> 4, (blockposition.getZ() - (z << 4)) >> 4));
+
+                if (x < r) {
+                    x++;
+                } else {
+                    z--;
+                }
+            }
+        }
+        return list;
+    }
+
+    public static int getTicketLevelFor(net.minecraft.world.level.chunk.ChunkStatus status) {
+        return net.minecraft.server.level.ChunkMap.MAX_VIEW_DISTANCE + net.minecraft.world.level.chunk.ChunkStatus.getDistance(status);
+    }
+}
