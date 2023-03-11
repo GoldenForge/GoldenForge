c1e7197af2268e1bd80b3bffe415126fd0699667
manugame <manugameur.pro@gmail.com>
2023-03-11 15:52:13 +0100

    Entity Activation 2.0 and optimize worldgen

--- src/main/java/org/goldenforge/GoldenConfig.java
+++ src/main/java/org/goldenforge/GoldenConfig.java
@@ -13,4 +13,34 @@ public class GoldenConfig {
     public static double globalMaxConcurrentLoads = 500.0;
     public static double playerMaxChunkLoadRate = -1.0;
 
+
+    // Activation Range
+
+    public static int animalActivationRange = 32;
+    public static int monsterActivationRange = 32;
+    public static int raiderActivationRange = 48;
+    public static int miscActivationRange = 16;
+    // Paper start
+    public static int flyingMonsterActivationRange = 32;
+    public static int waterActivationRange = 16;
+    public static int villagerActivationRange = 32;
+    public static int wakeUpInactiveAnimals = 4;
+    public static int wakeUpInactiveAnimalsEvery = 60*20;
+    public static int wakeUpInactiveAnimalsFor = 5*20;
+    public static int wakeUpInactiveMonsters = 8;
+    public static int wakeUpInactiveMonstersEvery = 20*20;
+    public static int wakeUpInactiveMonstersFor = 5*20;
+    public static int wakeUpInactiveVillagers = 4;
+    public static int wakeUpInactiveVillagersEvery = 30*20;
+    public static int wakeUpInactiveVillagersFor = 5*20;
+    public static int wakeUpInactiveFlying = 8;
+    public static int wakeUpInactiveFlyingEvery = 10*20;
+    public static int wakeUpInactiveFlyingFor = 5*20;
+    public static int villagersWorkImmunityAfter = 5*20;
+    public static int villagersWorkImmunityFor = 20;
+    public static boolean villagersActiveForPanic = true;
+    // Paper end
+    public static boolean tickInactiveVillagers = true;
+    public static boolean ignoreSpectatorActivation = false;
+
 }
