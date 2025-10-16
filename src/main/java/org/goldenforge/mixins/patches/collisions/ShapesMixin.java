8638da1dbb3ba1b2ac9fde867fbaf76177302674
manugame <braibant.manu@gmail.com>
2025-10-16 15:32:10 +0200

    patches cleanup

--- src/main/java/org/goldenforge/mixins/patches/collisions/ShapesMixin.java
+++ src/main/java/org/goldenforge/mixins/patches/collisions/ShapesMixin.java
@@ -1,10 +1,15 @@
 package org.goldenforge.mixins.patches.collisions;
 
+import ca.spottedleaf.moonrise.patches.collisions.CollisionUtil;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = Shapes.class, priority = 1001)
 public class ShapesMixin {
@@ -18,22 +23,48 @@ public class ShapesMixin {
         return ca.spottedleaf.moonrise.patches.collisions.CollisionUtil.joinOptimized(shape1, shape2, function); // Paper - optimise collisions
     }
 
+
     /**
-     * @author manugame_
-     * @reason Optimise collisions
+     * Use an inject instead of overwrite to avoid mixin conflicts - obviously this will still disregard any changes made by other
+     * mixins, but at least it won't conflict immediately. This is useful because some library mods mixin here when only the content
+     * mod actually needs the change.
+     *
+     * @reason Route to faster logic
+     * @author Spottedleaf
      */
-    @Overwrite
-    public static VoxelShape joinUnoptimized(VoxelShape shape1, VoxelShape shape2, BooleanOp function) {
-        return ca.spottedleaf.moonrise.patches.collisions.CollisionUtil.joinUnoptimized(shape1, shape2, function); // Paper - optimise collisions
+    @Inject(
+            method = "joinUnoptimized",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private static void injectJoinUnoptimized(final VoxelShape first, final VoxelShape second, final BooleanOp mergeFunction, final CallbackInfoReturnable<VoxelShape> cir) {
+        cir.setReturnValue(joinUnoptimized(first, second, mergeFunction));
+    }
+
+    @Unique
+    private static VoxelShape joinUnoptimized(final VoxelShape first, final VoxelShape second, final BooleanOp mergeFunction) {
+        final VoxelShape ret = CollisionUtil.joinUnoptimized(first, second, mergeFunction);
+        return ret;
     }
 
     /**
-     * @author manugame_
-     * @reason Optimise collisions
+     * @reason Route to faster logic
+     * @author Spottedleaf
+     * @see #injectJoinUnoptimized(VoxelShape, VoxelShape, BooleanOp, CallbackInfoReturnable)
      */
-    @Overwrite
-    public static boolean joinIsNotEmpty(VoxelShape shape1, VoxelShape shape2, BooleanOp resultOperator) {
-        return ca.spottedleaf.moonrise.patches.collisions.CollisionUtil.isJoinNonEmpty(shape1, shape2, resultOperator); // Paper - optimise collisions
+    @Inject(
+            method = "joinIsNotEmpty(Lnet/minecraft/world/phys/shapes/VoxelShape;Lnet/minecraft/world/phys/shapes/VoxelShape;Lnet/minecraft/world/phys/shapes/BooleanOp;)Z",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private static void injectJoinIsNotEmpty(final VoxelShape first, final VoxelShape second, final BooleanOp mergeFunction, final CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(joinIsNotEmpty(first, second, mergeFunction));
+    }
+
+    @Unique
+    private static boolean joinIsNotEmpty(final VoxelShape first, final VoxelShape second, final BooleanOp mergeFunction) {
+        final boolean ret = CollisionUtil.isJoinNonEmpty(first, second, mergeFunction);
+        return ret;
     }
 
 }
